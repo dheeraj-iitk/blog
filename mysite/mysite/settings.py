a783a23f75ab9f56f2a9ae39273e4525c64817ab
dheeraj-iitk <adheeraj>
2020-08-16 16:09:32 +0530

    edited settings

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -46,7 +46,6 @@ INSTALLED_APPS += ('django_summernote', )
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
